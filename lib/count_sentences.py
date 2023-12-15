14f36e8ce77c9564cbb6c6645063699f92620a84
Earl <earlmunyua96@gmail.com>
2023-12-15 13:40:00 +0300

    counts

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    
+    def __init__(self):
+        self._value = None
+
+    def get_value(self):
+        return self._value
+    
+    def set_value(self, value):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+        else:
+             self._value = value
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+    
+    def is_question(self):
+        return self._value.endswith("?")
+    
+    def is_exclamation(self):
+        return self._value.endswith("!")
+    
+    def count_sentences(self):
+        value_with_periods = self._value.replace('?', '.').replace('!', '.')
+        sentences = value_with_periods.split('.')
+        return len([sentence for sentence in sentences if sentence.strip()])
+    value = property(get_value, set_value)
+
+string = MyString()
+
+string.set_value("This is a sentence. And another one! What about this?")
+print(string.is_question())
+print(string.count_sentences())
+
+
+
+
